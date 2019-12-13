a33efa36cf01500e61f135099e3d00e84dd4a58b
yogeshwattamwar123 <52085907+yogeshwattamwar123@users.noreply.github.com>
2019-12-13 17:01:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var port = process.env.PORT || 3000;
 var url = "mongodb+srv://yogeshwattamwar1234:wattamwar_123@aptagrimdb-1nuqk.mongodb.net/test?retryWrites=true&w=majority";
 var app = express();
 
-  
+  app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://aptagrim-angular.herokuapp.com");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
+
   app.get("/api/getjobsdatas", function(req,res){
       mongoclient.connect(url,function(err,db){
           if(!err){
